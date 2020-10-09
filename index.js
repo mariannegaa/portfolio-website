6760af0f4e1c1e36a593b67aa1a062eebb1d5a38
Marianne Gaa <marianne.gaa@outlook.com>
2020-10-09 14:54:51 -0400

    navbar scroll shadow and headings font

---
+++ index.js
@@ -0,0 +1,9 @@
+// code taken from https://codepen.io/jasperreyes/pen/doqgdP
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 10) {
+        $('#navBar').addClass('floatingNav');
+    } else {
+        $('#navBar').removeClass('floatingNav');
+    }
+});
